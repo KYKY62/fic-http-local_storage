231b12efbda2a7ea522e7411b5d71ca2554b79d8
Your Name <you@example.com>
2022-12-04 23:52:01 +0700

    http login by pin finish

--- lib/module/exercise/ls_login_by_pin/controller/ls_login_by_pin_controller.dart
+++ lib/module/exercise/ls_login_by_pin/controller/ls_login_by_pin_controller.dart
@@ -21,6 +21,18 @@ class LsLoginByPinController extends State<LsLoginByPinView>
   String pin = "";
 
   updatePin(int number) async {
+    if (pin.length >= 4) return;
+    pin += number.toString();
+    setState(() {});
+
+    if (pin.length < 4) return;
+    String currentPin = await mainStorage.get("pin") ?? "";
+    if (pin != currentPin) {
+      showInfoDialog("Pin Salah!");
+      return;
+    }
+    await showInfoDialog("Berhasil Login!");
+    Get.back();
     /*
     TODO: --
     1. Buat if statement 
